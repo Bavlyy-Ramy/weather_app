23a79f92306640b0d9e46b5a8052901cb8464105
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-03-03 00:25:17 +0200

    setup

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_cubit.dart';
+import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/services/weather_service.dart';
+
+void main() {
+  runApp(
+    BlocProvider(
+      create: (context) => WeatherCubit(WeatherService()),
+      child: WeatherApp(),
+    ),
+  );
+}
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        // primarySwatch: Provider.of<WeatherProvider>(context).weatherData == null
+        //     ? Colors.blue
+        //     : Provider.of<WeatherProvider>(context)
+        //         .weatherData!
+        //         .getThemeColor(),
+      ),
+      home: HomePage(),
+    );
+  }
+}
